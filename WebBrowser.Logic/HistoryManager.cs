525766f9bcccb725e9156fa282ac5e017c8093f6
stevenmartinez123 <stevenmartinez1494@gmail.com>
2021-09-19 09:02:52 -0700

    History manager class created. Static methods for get history items and add history items created

---
+++ WebBrowser.Logic/HistoryManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBrowser.Data;
+using WebBrowser.Data.BrowserDataSetTableAdapters;
+
+namespace WebBrowser.Logic
+{
+    class HistoryManager
+    {
+
+        public static void AddHistoryItem(HistoryItem item)
+        {
+            var adapter = new HistoryTableAdapter();
+            adapter.Insert(item.URL, item.Title, item.Date);
+        }
+
+        public static List<HistoryItem> GetHistoryItems()
+        {
+            var adapter = new HistoryTableAdapter();
+            var results = new List<HistoryItem>();
+            var rows = adapter.GetData();
+
+                foreach (var row in rows)
+                {
+                    var item = new HistoryItem(row.URL, row.Title, row.Date);
+                    results.Add(item);
+            }
+            return results;
+        } 
+       
+        }
+
+    }
+
