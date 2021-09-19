3351dd3530e0be781f5561b22681a51e360b7c3c
stevenmartinez123 <stevenmartinez1494@gmail.com>
2021-09-19 09:12:28 -0700

    bookmark manager class created with static methods to add and get bookmark items from database

---
+++ WebBrowser.Logic/BookmarkManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBrowser.Data;
+using WebBrowser.Data.BrowserDataSetTableAdapters;
+
+namespace WebBrowser.Logic
+{
+    class BookmarkManager
+    {
+        public static void AddBookmarkItem(BookmarkItem item)
+        {
+            var adapter = new BookmarksTableAdapter();
+            adapter.Insert(item.URL, item.Title);
+        }
+
+        public static List<BookmarkItem> GetBookmarkItems()
+        {
+            var adapter = new BookmarksTableAdapter();
+            var results = new List<BookmarkItem>();
+            var rows = adapter.GetData();
+
+            foreach (var row in rows)
+            {
+                var item = new BookmarkItem();
+
+                item.URL = row.URL;
+                item.Title = row.Title;
+                results.Add(item);
+            }
+            return results; 
+        }
+
+    }
+}
