fac91cf31ef3449ef3d33fa5ddbd308ad2257ed3
stevenmartinez123 <stevenmartinez1494@gmail.com>
2021-09-18 21:10:34 -0700

    History Item class created with getters, setters

---
+++ WebBrowser.Logic/HistoryItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBrowser.Logic
+{
+    public class HistoryItem
+    {
+        public string URL { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        public HistoryItem(string URLin, string TitleIn, DateTime dateIn)
+        {
+            this.URL = URLin;
+            this.Title = TitleIn;
+            this.Date = dateIn;
+
+        }
+
+    }
+}
